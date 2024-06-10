fb7bbb262625034b557d47f3ff271c5bbb99dc0e
Henk Bornman <henk@synthesis.co.za>
2024-06-10 16:26:18 +0200

    default commit message

--- merge.py
+++ merge.py
@@ -1,36 +1,39 @@
 import os
 import json
-import sys
 
-def merge_json_files(directory : str, output_file: str, key: str):
+def merge_json_files():
 
-    output_file_path = os.path.join(directory, output_file)
+    SERVICE_ACCOUNT_PATH = "service_account/service_accounts.json"
+    TOPIC_PATH = "topic/topics.json"
 
-    with open(output_file_path, 'r') as f:
-        data = json.load(f)
+    with open(SERVICE_ACCOUNT_PATH, 'r') as f:
+        service_accounts = json.load(f)
     
-    for filename in os.listdir(directory):
-        if filename.endswith('.json') and filename != output_file:
-            file_path = os.path.join(directory, filename)
+    for filename in os.listdir("service_account"):
+        if filename.endswith('.json') and filename != "service_accounts.json" :
+            file_path = os.path.join("service_account", filename)
             with open(file_path, 'r') as f:
                 item = json.load(f)
-                data[key].append(item)
-            
+                service_accounts["service_accounts"].append(item)
             os.remove(file_path)
-            print(f"deleted file {file_path}")
-
     
-    with open(output_file_path, 'w') as f:
-        json.dump(data, f, indent=2)
+    with open(SERVICE_ACCOUNT_PATH, 'w') as f:
+        json.dump(service_accounts, f, indent=2)    
+    
+    with open(TOPIC_PATH, 'r') as f:
+        topics = json.load(f)
+    
+    for filename in os.listdir("topic"):
+        if filename.endswith('.json') and filename != "topics.json" :
+            file_path = os.path.join("topic", filename)
+            with open(file_path, 'r') as f:
+                item = json.load(f)
+                topics["topics"].append(item)
+            os.remove(file_path)
+    
+    with open(TOPIC_PATH, 'w') as f:
+        json.dump(topics, f, indent=2)
 
 
 if __name__ == "__main__":
-    directory = sys.argv[1]
-    
-    match directory:
-        case "topic":
-            merge_json_files("topic", "topics.json", "topics")
-        case "service_account":
-            merge_json_files("service_account", "service_accounts.json", "service_accounts")
-        case _:
-            print("")
+    merge_json_files()
